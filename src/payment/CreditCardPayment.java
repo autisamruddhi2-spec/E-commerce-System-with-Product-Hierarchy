0161056c562f985b7ea25cf9dbf30f81d1ad46ff
autisamruddhi2-spec <autisamruddhi2@gmail.com>
2026-02-14 16:48:09 +0530

    Create CreditCardPayment.java

---
+++ src/payment/CreditCardPayment.java
@@ -0,0 +1,11 @@
+package payment;
+
+
+public class CreditCardPayment implements PaymentMethod {
+
+    @Override
+    public boolean pay(double amount) {
+        System.out.println("Paid ₹" + amount + " using Credit Card.");
+        return true;
+    }
+}
