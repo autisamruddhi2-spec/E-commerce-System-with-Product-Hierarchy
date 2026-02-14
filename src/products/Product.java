bb521a3cd193be30a351272608b916c3afe90a06
autisamruddhi2-spec <autisamruddhi2@gmail.com>
2026-02-14 16:43:30 +0530

    Create Product.java

---
+++ src/products/Product.java
@@ -0,0 +1,37 @@
+package products;
+
+public abstract class Product {
+    protected String id;
+    protected String name;
+    protected double price;
+    protected String description;
+    protected int stockQuantity;
+
+    public Product(String id, String name, double price,
+                   String description, int stockQuantity) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.stockQuantity = stockQuantity;
+    }
+
+    public abstract double calculateDiscount();
+
+    public double getFinalPrice() {
+        return price - calculateDiscount();
+    }
+
+    public String getId() { return id; }
+    public String getName() { return name; }
+    public int getStockQuantity() { return stockQuantity; }
+    public void reduceStock(int quantity) { stockQuantity -= quantity; }
+
+    public void displayInfo() {
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Price: ₹" + price);
+        System.out.println("Discount: ₹" + calculateDiscount());
+        System.out.println("Final Price: ₹" + getFinalPrice());
+    }
+}
